99bf55557924c3b030d1b78748998274039f9d3e
Aman Shukla <amanks7400@gmail.com>
2022-04-16 18:45:13 +0530

    Add Initial Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
 import { Button } from '@mui/material';
 import React from 'react';
 
 const App = () => {
   return (
     <div>
-      Welcome to Examily
-      <Button>Next</Button>
+      <Routes>
+        <Route path='/' exact element={<p>Wekcome to Examily</p>} />
+        <Route path='/landing' element={<p>Landing</p>} />
+        <Route path='/login' element={<p>Login</p>} />
+        <Route path='/register' element={<p>Register</p>} />
+      </Routes>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import { ThemeProvider, createTheme } from '@mui/material';
 
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import themeOptions from '../src/utils/theme';
@@ -11,8 +12,10 @@ const theme = createTheme(themeOptions);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
