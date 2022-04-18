9021cde9019590a16ddd79e7a69dca21340545f9
Aman Shukla <amanks7400@gmail.com>
2022-04-18 12:31:28 +0530

    Laning Hero Section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,9 +4,24 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
 const PageContainer = styled('div')(({ theme }) => ({
-  padding: theme.spacing(6),
+  padding: theme.spacing(10),
   height: '100vh',
 }));
+
+const HeroImageContainer = styled('section')({
+  flex: 1,
+  display: 'grid',
+  placeContent: 'end',
+  '>img': {
+    maxWidth: '100%',
+    height: 'auto',
+  },
+});
+const HeroTextContainer = styled('section')({
+  flex: 1,
+
+  marginTop: '5rem',
+});
 
 const Landing = () => {
   return (
@@ -56,6 +71,41 @@ const Landing = () => {
         </Stack>
       </Stack>
       {/* Body */}
+      <Stack direction='row' sx={{ my: 10 }}>
+        {/* Hero Text */}
+        <HeroTextContainer>
+          <Typography
+            variant='h2'
+            sx={{
+              fontWeight: 'bold',
+              '>span': {
+                color: 'secondary.main',
+              },
+            }}
+          >
+            Want to create a <br /> model <span>test</span> or <span>quiz</span>{' '}
+            ?
+          </Typography>
+          <Typography variant='h4' sx={{ color: '#c1bebe', my: 2 }}>
+            Create tests on our platform for free. <br />
+            Sell your MCQs tests and track results
+          </Typography>
+          {/* CTA's */}
+          <Stack direction='row' spacing={2}>
+            <Button variant='contained' color='secondary' size='large'>
+              Create A Test
+            </Button>
+            <Button variant='outlined' color='secondary' size='large'>
+              Take A Test
+            </Button>
+          </Stack>
+        </HeroTextContainer>
+
+        {/* Hero Image */}
+        <HeroImageContainer>
+          <img src='/assets/landing-hero.png' alt='Hero' />
+        </HeroImageContainer>
+      </Stack>
     </PageContainer>
   );
 };
